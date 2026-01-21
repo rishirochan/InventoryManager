1bd3f457e593356fe0fbc7cb30672b2696586365
Rishi Potipireddi <rpotipir@uci.edu>
2026-01-20 21:01:13 -0800

    Fix CORS: Restrict origins to Vercel and Localhost

--- src/main.py
+++ src/main.py
@@ -33,7 +33,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://inventory-manager-iota-livid.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"],
